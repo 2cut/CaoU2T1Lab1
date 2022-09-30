8eae23d1df6b74f8e9a779daaf4baf1b23c3f37d
2cut <jialiangc@nycstudents.net>
2022-09-29 21:50:16 -0400

    finished lab

--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,5 +1,11 @@
 public class CatRunner {
     public static void main(String[] args) {
-        Cat cat1 = new Cat("boop", 5, 20.5);
+        Cat cat1 = new Cat("boop", 8, 20.5);
+        cat1.introduce();
+        cat1.printCatInfo();
+
+        Cat cat2 = new Cat("Meowsicles", 2, 9.3);
+        cat2.introduce();
+        cat2.printCatInfo();
     }
 }
